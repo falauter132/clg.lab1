1d1029ccf6d5ae6902f985aab1d9525bbb0b7b0f
Ostafi SASHA <maricelostafi79@gmail.com>
2024-11-15 10:10:40 +0200

    Добавьте файлы проекта.

---
+++ lab5/lab5.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cin >> x;
+    cin >> y;
+
+    double result = (x * y) + ((x + y ^ 2 + 3) / (x ^ 2 + 5));
+
+    cout << result;
+}
+
+///////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int a;
+    cin >> a;
+
+    double y = cos(a) + cos(2 * a) + cos(6 * a) + cos(7 * a);
+    double z = (4 * cos(a / 2)) * cos(5 / 2 * a) * cos(4 * a);
+
+    cout << y, "\n", z;
+}
